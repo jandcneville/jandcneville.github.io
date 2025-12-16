93b4c7820ea5eb3eb15ecacb78467b8ed88f1b32
Bradley <bradleyneville11@gmail.com>
2025-12-16 00:37:04 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react()
-  ],
+export default defineConfig({
+  plugins: [react()],
+  base: "/", // correct for jandcneville.github.io
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-}));
+});
