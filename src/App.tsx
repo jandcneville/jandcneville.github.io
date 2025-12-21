63c7132d99c2cf6c0fc75b01d3dc9ba17434a444
Bradley <bradleyneville11@gmail.com>
2025-12-21 19:54:20 +0000

    Scroll to top on page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import Products from "./pages/Products";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "@/components/ScrollToTop";
+
 
 const queryClient = new QueryClient();
 
@@ -17,6 +19,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+      <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/products" element={<Products />} />
